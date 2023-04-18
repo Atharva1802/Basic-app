31da86e6fe4fec950ba49822fba2b10c2a48aa82
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-04-18 23:30:21 +0530

    Cards Page linked

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About.jsx';
+import Cards from './pages/Cards';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cards" element={<Cards />} />
+
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
+
+export default App;
